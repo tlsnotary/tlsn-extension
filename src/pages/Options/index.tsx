2595eabd7759c5a2ef4520d73b5e0e7d00e8b469
0xtsukino <0xtsukino@gmail.com>
2023-08-07 15:22:53 -0700

    updated bp

--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -5,5 +5,5 @@ import Options from './Options';
 import './index.css';
 
 const container = document.getElementById('app-container');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(<Options title={'Settings'} />);
---
+++ src/pages/Popup/Popup.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/img/icon-128.png';
+import './Popup.css';
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+};
+
+export default Popup;
