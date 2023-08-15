3d22f4f06eb33e516c818f2c3dcb274f33947d13
0xtsukino <0xtsukino@gmail.com>
2023-08-14 21:16:57 -0700

    include wasm

---
+++ wasm/hello_wasm/hello_wasm_bg.js
@@ -0,0 +1,35 @@
+let wasm;
+export function __wbg_set_wasm(val) {
+    wasm = val;
+}
+
+
+const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
+
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachedUint8Memory0 = null;
+
+function getUint8Memory0() {
+    if (cachedUint8Memory0 === null || cachedUint8Memory0.byteLength === 0) {
+        cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+/**
+*/
+export function greet() {
+    wasm.greet();
+}
+
+export function __wbg_alert_8755b7883b6ce0ef(arg0, arg1) {
+    alert(getStringFromWasm0(arg0, arg1));
+};
+
