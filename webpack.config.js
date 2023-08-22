a1053a12eb9f9b10ee346bc739785a39d1444aef
0xtsukino <0xtsukino@gmail.com>
2023-08-21 17:50:45 -0700

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,11 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 
 var options = {
   mode: process.env.NODE_ENV || "development",
-  ignoreWarnings: [/Circular dependency between chunks with runtime/],
+  ignoreWarnings: [
+      /Circular dependency between chunks with runtime/,
+      /ResizeObserver loop completed with undelivered notifications/
+  ],
+
   entry: {
     options: path.join(__dirname, "src", "pages", "Options", "index.tsx"),
     popup: path.join(__dirname, "src", "pages", "Popup", "index.tsx"),
