289494399cd354752ef9eff404939d98e4344c46
0xtsukino <0xtsukino@gmail.com>
2023-08-24 19:02:14 -0700

    fix errors

--- src/pages/Offscreen/worker.ts
+++ src/pages/Offscreen/worker.ts
@@ -1,25 +1,33 @@
 import * as Comlink from 'comlink';
 import init, { prover } from '../../../wasm/prover/pkg/tlsn_extension_rs';
 
-class Test {
+class TLSN {
   constructor() {
     console.log('worker test module initiated.');
-    this.test();
   }
 
-  async test() {
-    console.log('start');
-    console.log(
-      '!@# navigator.hardwareConcurrency=',
-      navigator.hardwareConcurrency,
-    );
-    await init();
-    // await initThreadPool(2);
-    // console.log("!@# result js=", DATA.reduce((sum, n) => sum + n, 0));
-    // console.log("!@# result rs=", sum(new Int32Array(DATA)));
-    const resProver = await prover();
-    console.log('!@# resProver=', resProver);
+  async prover() {
+    try {
+      console.log('start');
+      console.log(
+        '!@# navigator.hardwareConcurrency=',
+        navigator.hardwareConcurrency,
+      );
+      await init();
+      // await initThreadPool(2);
+      // console.log("!@# result js=", DATA.reduce((sum, n) => sum + n, 0));
+      // console.log("!@# result rs=", sum(new Int32Array(DATA)));
+      console.log('!@# reqProver=');
+      const resProver = await prover();
+      console.log('!@# resProver=', resProver);
+
+      return 'resProver';
+    } catch (e: any) {
+      console.log(e);
+      return e;
+    }
+
   }
 }
 
-Comlink.expose(Test);
+Comlink.expose(TLSN);
