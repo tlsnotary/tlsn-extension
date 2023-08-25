abbad3f1c0190c0cfdefed26a6ce01c96aee7762
0xtsukino <0xtsukino@gmail.com>
2023-08-24 19:46:29 -0700

    add github action

--- src/pages/Offscreen/Offscreen.tsx
+++ src/pages/Offscreen/Offscreen.tsx
@@ -2,35 +2,30 @@ import React, { useEffect } from 'react';
 import * as Comlink from 'comlink';
 import { BackgroundActiontype } from '../Background/actionTypes';
 
-
-
 const Offscreen = () => {
   useEffect(() => {
     (async function offscreenloaded() {
       console.log('offscreen loaded - spawning worker from worker.ts');
 
+      chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        switch (request.type) {
+          case BackgroundActiontype.test_wasm: {
+            const TLSN: any = Comlink.wrap(
+              new Worker(new URL('./worker.ts', import.meta.url)),
+            );
 
-      chrome.runtime.onMessage.addListener(
-        (request, sender, sendResponse) => {
-          switch (request.type) {
-            case BackgroundActiontype.test_wasm: {
-              const TLSN: any = Comlink.wrap(
-                new Worker(new URL('./worker.ts', import.meta.url)),
-              );
+            new TLSN().then(async (tlsn: any) => {
+              const data = await tlsn.prover();
+              sendResponse({ data });
+            });
 
-              new TLSN().then(async (tlsn: any) => {
-                const data = await tlsn.prover();
-                sendResponse({data});
-              });
-
-              break;
-            }
-            default:
-              break;
+            break;
           }
-          return true;
-        },
-      );
+          default:
+            break;
+        }
+        return true;
+      });
     })();
   }, []);
 
