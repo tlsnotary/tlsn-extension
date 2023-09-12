a476478cc1bbae4c85bfab6ae84d5641ca0f9ccd
mhchia <kevin.mh.chia@gmail.com>
2023-09-12 16:42:43 +0800

    lint

--- src/pages/Offscreen/worker.ts
+++ src/pages/Offscreen/worker.ts
@@ -1,5 +1,8 @@
 import * as Comlink from 'comlink';
-import init, { initThreadPool, prover } from '../../../wasm/prover/pkg/tlsn_extension_rs';
+import init, {
+  initThreadPool,
+  prover,
+} from '../../../wasm/prover/pkg/tlsn_extension_rs';
 
 class TLSN {
   constructor() {
@@ -10,20 +13,23 @@ class TLSN {
     try {
       console.log('start');
       const numConcurrency = navigator.hardwareConcurrency;
-      console.log(
-        '!@# navigator.hardwareConcurrency=',
-        numConcurrency,
-      );
+      console.log('!@# navigator.hardwareConcurrency=', numConcurrency);
       const res = await init();
-      console.log("!@# res.memory=", res.memory)
+      console.log('!@# res.memory=', res.memory);
       // 6422528 ~= 6.12 mb
-      console.log("!@# res.memory.buffer.length=", res.memory.buffer.byteLength)
+      console.log(
+        '!@# res.memory.buffer.length=',
+        res.memory.buffer.byteLength,
+      );
       await initThreadPool(numConcurrency);
       const resProver = await prover();
-      console.log("!@# resProver=", resProver)
-      console.log("!@# resAfter.memory=", res.memory)
+      console.log('!@# resProver=', resProver);
+      console.log('!@# resAfter.memory=', res.memory);
       // 1105920000 ~= 1.03 gb
-      console.log("!@# resAfter.memory.buffer.length=", res.memory.buffer.byteLength)
+      console.log(
+        '!@# resAfter.memory.buffer.length=',
+        res.memory.buffer.byteLength,
+      );
 
       return resProver;
     } catch (e: any) {
