6beada88b5cfae21192052e2b52c5f7a5e7ae6b5
mhchia <kevin.mh.chia@gmail.com>
2023-09-23 01:29:36 +0800

    lint

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import * as Comlink from 'comlink';
 // import init, { prover } from "./pkg/tlsn_extension_rs";
 import init, { initThreadPool, prover } from './pkg/tlsn_extension_rs';
@@ -7,12 +8,19 @@ function hasSharedMemory() {
   const hasSharedArrayBuffer = 'SharedArrayBuffer' in global;
 =======
 import * as Comlink from "comlink";
+=======
+import * as Comlink from 'comlink';
+>>>>>>> 07ac764 (lint)
 // import init, { prover } from "./pkg/tlsn_extension_rs";
-import init, { initThreadPool, prover } from "./pkg/tlsn_extension_rs";
+import init, { initThreadPool, prover } from './pkg/tlsn_extension_rs';
 
 function hasSharedMemory() {
+<<<<<<< HEAD
   const hasSharedArrayBuffer = "SharedArrayBuffer" in global;
 >>>>>>> 7462ae9 (Use the latest tlsn@8b163540 with patch)
+=======
+  const hasSharedArrayBuffer = 'SharedArrayBuffer' in global;
+>>>>>>> 07ac764 (lint)
   const notCrossOriginIsolated = global.crossOriginIsolated === false;
 
   return hasSharedArrayBuffer && !notCrossOriginIsolated;
@@ -22,10 +30,14 @@ const DATA = Array(20).fill(1);
 
 class Test {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 07ac764 (lint)
   constructor() {
     console.log('!@# test comlink');
     this.test();
   }
+<<<<<<< HEAD
 
   async test() {
     console.log('start');
@@ -54,24 +66,32 @@ Comlink.expose(Test);
         console.log('!@# test comlink');
         this.test();
     }
+=======
+>>>>>>> 07ac764 (lint)
 
-    async test() {
-        console.log('start');
-        console.log("!@# hasSharedMemory=", hasSharedMemory())
-        const numConcurrency = navigator.hardwareConcurrency;
-        console.log("!@# numConcurrency=", numConcurrency)
-        const res = await init();
-        console.log("!@# res.memory=", res.memory)
-        // 6422528 ~= 6.12 mb
-        console.log("!@# res.memory.buffer.length=", res.memory.buffer.byteLength)
-        await initThreadPool(numConcurrency);
-        const resProver = await prover();
-        console.log("!@# resProver=", resProver)
-        console.log("!@# resAfter.memory=", res.memory)
-        // 1105920000 ~= 1.03 gb
-        console.log("!@# resAfter.memory.buffer.length=", res.memory.buffer.byteLength)
-    }
+  async test() {
+    console.log('start');
+    console.log('!@# hasSharedMemory=', hasSharedMemory());
+    const numConcurrency = navigator.hardwareConcurrency;
+    console.log('!@# numConcurrency=', numConcurrency);
+    const res = await init();
+    console.log('!@# res.memory=', res.memory);
+    // 6422528 ~= 6.12 mb
+    console.log('!@# res.memory.buffer.length=', res.memory.buffer.byteLength);
+    await initThreadPool(numConcurrency);
+    const resProver = await prover();
+    console.log('!@# resProver=', resProver);
+    console.log('!@# resAfter.memory=', res.memory);
+    // 1105920000 ~= 1.03 gb
+    console.log(
+      '!@# resAfter.memory.buffer.length=',
+      res.memory.buffer.byteLength,
+    );
+  }
 }
 
 Comlink.expose(Test);
+<<<<<<< HEAD
 >>>>>>> 7462ae9 (Use the latest tlsn@8b163540 with patch)
+=======
+>>>>>>> 07ac764 (lint)
