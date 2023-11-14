c6165bafcc65b98c4b15c9f648826f28f6eda556
0xtsukino <0xtsukino@gmail.com>
2023-11-14 15:35:13 +0300

    basic plugin for twitter and reddit

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,5 @@
+import {RequestLog} from "../pages/Background/actionTypes";
+
 export function urlify(
   text: string,
   params?: [string, string, boolean?][],
@@ -37,4 +39,42 @@ export function download(filename: string, content: string) {
   element.click();
 
   document.body.removeChild(element);
+}
+
+export async function replayRequest(req: RequestLog): Promise<string> {
+  const options = {
+    method: req.method,
+    headers: req.requestHeaders.reduce(
+      (acc: { [key: string]: string }, h: chrome.webRequest.HttpHeader) => {
+        if (typeof h.name !== 'undefined' && typeof h.value !== 'undefined') {
+          acc[h.name] = h.value;
+        }
+        return acc;
+      },
+      {},
+    ),
+    body: req.requestBody,
+  };
+
+  if (req?.formData) {
+    const formData = new URLSearchParams();
+    Object.entries(req.formData).forEach(([key, values]) => {
+      values.forEach((v) => formData.append(key, v));
+    });
+    options.body = formData.toString();
+  }
+
+  const resp = await fetch(req.url, options);
+  const contentType =
+    resp?.headers.get('content-type') || resp?.headers.get('Content-Type');
+
+  if (contentType?.includes('application/json')) {
+    return resp.text();
+  } else if (contentType?.includes('text')) {
+    return resp.text();
+  } else if (contentType?.includes('image')) {
+    return resp.blob().then((blob) => blob.text());
+  } else {
+    return resp.blob().then((blob) => blob.text());
+  }
 }
