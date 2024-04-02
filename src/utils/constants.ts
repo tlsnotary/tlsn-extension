d55279501e61ed208bbef3b09cb482f9a5d5a2c9
Tanner Shaw <traumaaa@gmail.com>
2024-04-02 12:15:52 -0700

    refactor: just changing to .5 now, can push when released

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const EXPLORER_API = 'http://localhost:3000';
-export const NOTARY_API = 'https://notary.pse.dev/v0.1.0-alpha.4';
+export const NOTARY_API = 'https://notary.pse.dev/v0.1.0-alpha.5';
