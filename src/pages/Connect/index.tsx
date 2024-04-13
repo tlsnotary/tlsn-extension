3d8aec4083dd2b30365878e636d3bb8088fe43b6
tsukino <0xtsukino@gmail.com>
2024-04-13 16:51:27 +0200

    fix: styling change

--- src/pages/Connect/index.tsx
+++ src/pages/Connect/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState, useCallback} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { sendPairRequest } from '../../reducers/p2p';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { usePairId } from '../../reducers/p2p';
-
+import Icon from '../../components/Icon';
 
 export default function Connect() {
   const dispatch = useDispatch();
@@ -19,12 +19,11 @@ export default function Connect() {
       setLoading(false);
       navigate('/create-session');
     }
-
   }, [pairId]);
 
   const connect = useCallback(() => {
     if (peerId) {
-      console.log('Connecting to peer', peerId)
+      console.log('Connecting to peer', peerId);
       dispatch(sendPairRequest(peerId));
       setLoading(true);
     } else {
@@ -32,17 +31,34 @@ export default function Connect() {
     }
   }, [peerId]);
 
-
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1>Enter peer ID to connect to</h1>
+    <div className="flex flex-col justify-center items-center bg-slate-200 p-4 rounded border-slate-400 m-4 gap-2">
+      <h1 className="text-base font-semibold">Enter peer ID to connect to</h1>
       <input
+        className="input border border-slate-200 focus:border-slate-400 w-full"
         type="text"
         value={peerId}
         onChange={(e) => setPeerId(e.target.value)}
-        onKeyDown={connect}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') connect();
+        }}
+        autoFocus
       />
-      <button disabled={!peerId} onClick={connect}>Connect</button>
+      <button
+        className="button button--primary"
+        disabled={!peerId || loading}
+        onClick={connect}
+      >
+        {loading ? (
+          <Icon
+            className="animate-spin text-white"
+            fa="fa-solid fa-spinner"
+            size={1}
+          />
+        ) : (
+          'Connect'
+        )}
+      </button>
     </div>
   );
 }
