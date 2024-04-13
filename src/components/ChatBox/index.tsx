8ad4c0b4f8d595c2b034d1d1e27737503890dc8b
John Shaw <codetrauma@Johns-MacBook-Pro.local>
2024-04-13 16:51:52 +0200

    wip

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -27,8 +27,17 @@ export default function ChatBox() {
       );
 
       setText('');
+
+      console.log('after sending')
     }
   }, [text, pairId]);
+
+  const handleKeyDown = useCallback((e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' && text && pairId) {
+      onSend();
+      setText('');
+    }
+  }, [text]);
 
   return (
     <div className="flex flex-col flex-nowrap flex-grow gap-1 p-2">
@@ -69,6 +78,9 @@ export default function ChatBox() {
           <input
             className="input border border-slate-200 focus:border-slate-400 flex-grow p-2"
             onChange={(e) => setText(e.target.value)}
+            onKeyDown={handleKeyDown}
+            value={text}
+            
           />
           <button
             className={classNames('button', {
