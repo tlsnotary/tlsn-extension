eaeb2cfb7c6944e93d075d0bf9c305b596ac5715
tsukino <0xtsukino@gmail.com>
2024-04-13 16:11:38 +0200

    wip: consolidate logic to ChatBox

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -1,13 +1,20 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { sendChat, useChatMessages, useClientId } from '../../reducers/p2p';
+import {
+  sendChat,
+  useChatMessages,
+  useClientId,
+  usePairId,
+} from '../../reducers/p2p';
 import { useDispatch } from 'react-redux';
 import classNames from 'classnames';
+import Icon from '../Icon';
 
 export default function ChatBox() {
   const messages = useChatMessages();
   const dispatch = useDispatch();
   const clientId = useClientId();
   const [text, setText] = useState('');
+  const pairId = usePairId();
 
   const onSend = useCallback(() => {
     if (text) {
@@ -24,26 +31,40 @@ export default function ChatBox() {
   }, [text]);
 
   return (
-    <div className="flex flex-col h-full gap-1">
-      <div className="flex flex-col border border-slate-200 flex-grow overflow-y-auto">
-        {messages.map((msg) => {
-          return <div>{msg.text}</div>;
-        })}
+    <div className="flex flex-col flex-nowrap flex-grow gap-1 p-2">
+      <div className="flex flex-row gap-1 font-semibold text-xs align-center">
+        <div>Client ID:</div>
+        {clientId ? (
+          <div className="text-green-500">{clientId}</div>
+        ) : (
+          <Icon
+            className="animate-spin text-gray-500"
+            fa="fa-solid fa-spinner"
+            size={1}
+          />
+        )}
       </div>
-      <div className="flex flex-row w-full gap-1">
-        <input
-          className="input border border-slate-200 focus:border-slate-400 flex-grow p-2"
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button
-          className={classNames('button', {
-            'button--primary': !!text,
+      <div className="flex flex-col h-full gap-1">
+        <div className="flex flex-col border border-slate-200 flex-grow overflow-y-auto">
+          {messages.map((msg) => {
+            return <div>{msg.text}</div>;
           })}
-          disabled={!text}
-          onClick={onSend}
-        >
-          Send
-        </button>
+        </div>
+        <div className="flex flex-row w-full gap-1">
+          <input
+            className="input border border-slate-200 focus:border-slate-400 flex-grow p-2"
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button
+            className={classNames('button', {
+              'button--primary': !!text && !!pairId,
+            })}
+            disabled={!text || !pairId}
+            onClick={onSend}
+          >
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/CreateSession/index.tsx
+++ src/pages/CreateSession/index.tsx
@@ -1,39 +1,15 @@
 import type {} from 'redux-thunk/extend-redux';
 import React, { ReactElement, useEffect } from 'react';
-import {
-  connectSession,
-  useClientId,
-  useConnected,
-  useSocket,
-} from '../../reducers/p2p';
+import { connectSession } from '../../reducers/p2p';
 import { useDispatch } from 'react-redux';
-import Icon from '../../components/Icon';
 import ChatBox from '../../components/ChatBox';
 
 export default function CreateSession(): ReactElement {
-  const clientId = useClientId();
   const dispatch = useDispatch();
-  const connected = useConnected();
 
   useEffect(() => {
     dispatch(connectSession());
   }, []);
 
-  return (
-    <div className="flex flex-col flex-nowrap flex-grow gap-1 p-2">
-      <div className="flex flex-row gap-1 font-semibold text-xs align-center">
-        <div>Client ID:</div>
-        {clientId ? (
-          <div className="text-green-500">{clientId}</div>
-        ) : (
-          <Icon
-            className="animate-spin text-gray-500"
-            fa="fa-solid fa-spinner"
-            size={1}
-          />
-        )}
-      </div>
-      {connected && <ChatBox />}
-    </div>
-  );
+  return <ChatBox />;
 }
