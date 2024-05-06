5b811ee0b2dff093909d2ba43bac82a34a492f25
Tanner Shaw <traumaaa@gmail.com>
2024-05-06 13:22:04 -0700

    feature: setting default values for max_sent and max_recv

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const EXPLORER_API = 'https://explorer.tlsnotary.org';
 export const NOTARY_API = 'https://notary.pse.dev/v0.1.0-alpha.5';
 export const NOTARY_PROXY = 'wss://notary.pse.dev/proxy';
-export const MAX_RECV = 1024;
-export const MAX_SENT = 1024;
+export const MAX_RECV = 16384;
+export const MAX_SENT = 4096;
