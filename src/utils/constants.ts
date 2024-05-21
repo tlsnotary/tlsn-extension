c3240f5bc8961b9d0e704df94f0368acaa8180fa
tsukino <0xtsukino@gmail.com>
2024-05-21 22:46:41 +0800

    feat: add logging options and reload extension on save

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,7 @@ export const NOTARY_API = 'https://notary.pse.dev/v0.1.0-alpha.5';
 export const NOTARY_PROXY = 'wss://notary.pse.dev/proxy';
 export const MAX_RECV = 16384;
 export const MAX_SENT = 4096;
+export const LOGGING_LEVEL_NONE = ' ';
+export const LOGGING_LEVEL_INFO = 'info,tlsn_extension_rs=info';
+export const LOGGING_LEVEL_DEBUG = 'debug,tlsn_extension_rs=debug';
+export const LOGGING_LEVEL_TRACE = 'trace,tlsn_extension_rs=trace';
