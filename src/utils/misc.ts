9f4de6697f91b82d0af265d7743bb990f008bddc
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-06-11 16:58:03 +0200

    fix: add body to request

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -210,6 +210,7 @@ export const makePlugin = async (
         const resp = await fetch(url, {
           method,
           headers,
+          body: reqBody,
         });
         const body = await extractBodyFromResponse(resp);
 
