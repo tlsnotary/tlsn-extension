945617eb8ac9815bdab998afa62c68427d4b1cca
tsukino <0xtsukino@gmail.com>
2024-08-13 20:39:01 +0800

    feat: integrate with alpha.6

--- src/entries/Offscreen/Offscreen.tsx
+++ src/entries/Offscreen/Offscreen.tsx
@@ -1,154 +1,117 @@
 import React, { useEffect } from 'react';
-import { BackgroundActiontype } from '../Background/rpc';
-import { prove, set_logging_filter, verify } from 'tlsn-js';
-import { urlify } from '../../utils/misc';
-import browser from 'webextension-polyfill';
-import { LOGGING_LEVEL_INFO } from '../../utils/constants';
+import * as Comlink from 'comlink';
 import { OffscreenActionTypes } from './types';
+import {
+  NotaryServer,
+  Prover as _Prover,
+  NotarizedSession as _NotarizedSession,
+  TlsProof as _TlsProof,
+} from 'tlsn-js';
+import { verify } from 'tlsn-jsV5.3';
+
+import { urlify } from '../../utils/misc';
+import { BackgroundActiontype } from '../Background/rpc';
+import browser from 'webextension-polyfill';
+import { Proof, ProofV1 } from '../../utils/types';
+
+const { init, Prover, NotarizedSession, TlsProof }: any = Comlink.wrap(
+  new Worker(new URL('./worker.ts', import.meta.url)),
+);
 
 const Offscreen = () => {
   useEffect(() => {
-    // @ts-ignore
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      switch (request.type) {
-        case OffscreenActionTypes.notarization_request: {
-          const {
-            url,
-            method,
-            headers,
-            body = '',
-            maxSentData,
-            maxRecvData,
-            maxTranscriptSize,
-            notaryUrl,
-            websocketProxyUrl,
-            id,
-            secretHeaders,
-            secretResps,
-            loggingFilter = LOGGING_LEVEL_INFO,
-          } = request.data;
+    (async () => {
+      await init({ loggingLevel: 'Info' });
+      // @ts-ignore
+      chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        switch (request.type) {
+          case OffscreenActionTypes.notarization_request: {
+            const { id } = request.data;
 
-          (async () => {
-            try {
-              const token = urlify(url)?.hostname || '';
-              await set_logging_filter(loggingFilter);
-              const proof = await prove(url, {
-                method,
-                headers,
-                body,
-                maxSentData,
-                maxRecvData,
-                maxTranscriptSize,
-                notaryUrl,
-                websocketProxyUrl: websocketProxyUrl + `?token=${token}`,
-                secretHeaders,
-                secretResps,
-              });
+            (async () => {
+              try {
+                const proof = await createProof(request.data);
 
-              browser.runtime.sendMessage({
-                type: BackgroundActiontype.finish_prove_request,
-                data: {
-                  id,
-                  proof,
-                },
-              });
+                browser.runtime.sendMessage({
+                  type: BackgroundActiontype.finish_prove_request,
+                  data: {
+                    id,
+                    proof,
+                  },
+                });
 
-              browser.runtime.sendMessage({
-                type: OffscreenActionTypes.notarization_response,
-                data: {
-                  id,
-                  proof,
-                },
-              });
-            } catch (error) {
-              console.error(error);
-              browser.runtime.sendMessage({
-                type: BackgroundActiontype.finish_prove_request,
-                data: {
-                  id,
-                  error,
-                },
-              });
+                browser.runtime.sendMessage({
+                  type: OffscreenActionTypes.notarization_response,
+                  data: {
+                    id,
+                    proof,
+                  },
+                });
+              } catch (error) {
+                console.error(error);
+                browser.runtime.sendMessage({
+                  type: BackgroundActiontype.finish_prove_request,
+                  data: {
+                    id,
+                    error,
+                  },
+                });
 
-              browser.runtime.sendMessage({
-                type: OffscreenActionTypes.notarization_response,
-                data: {
-                  id,
-                  error,
-                },
-              });
-            }
-          })();
+                browser.runtime.sendMessage({
+                  type: OffscreenActionTypes.notarization_response,
+                  data: {
+                    id,
+                    error,
+                  },
+                });
+              }
+            })();
 
-          break;
-        }
-        case BackgroundActiontype.process_prove_request: {
-          const {
-            url,
-            method,
-            headers,
-            body = '',
-            maxSentData,
-            maxRecvData,
-            maxTranscriptSize,
-            notaryUrl,
-            websocketProxyUrl,
-            id,
-            secretHeaders,
-            secretResps,
-            loggingFilter = LOGGING_LEVEL_INFO,
-          } = request.data;
+            break;
+          }
+          case BackgroundActiontype.process_prove_request: {
+            const { id } = request.data;
 
-          (async () => {
-            try {
-              const token = urlify(url)?.hostname || '';
-              await set_logging_filter(loggingFilter);
-              const proof = await prove(url, {
-                method,
-                headers,
-                body,
-                maxSentData,
-                maxRecvData,
-                maxTranscriptSize,
-                notaryUrl,
-                websocketProxyUrl: websocketProxyUrl + `?token=${token}`,
-                secretHeaders,
-                secretResps,
-              });
+            (async () => {
+              try {
+                const proof = await createProof(request.data);
 
-              browser.runtime.sendMessage({
-                type: BackgroundActiontype.finish_prove_request,
-                data: {
-                  id,
-                  proof,
-                },
-              });
-            } catch (error) {
-              browser.runtime.sendMessage({
-                type: BackgroundActiontype.finish_prove_request,
-                data: {
-                  id,
-                  error,
-                },
-              });
-            }
-          })();
+                browser.runtime.sendMessage({
+                  type: BackgroundActiontype.finish_prove_request,
+                  data: {
+                    id,
+                    proof: proof,
+                  },
+                });
+              } catch (error) {
+                console.error(error);
+                browser.runtime.sendMessage({
+                  type: BackgroundActiontype.finish_prove_request,
+                  data: {
+                    id,
+                    error,
+                  },
+                });
+              }
+            })();
 
-          break;
-        }
-        case BackgroundActiontype.verify_proof: {
-          (async () => {
-            const result = await verify(request.data);
-            sendResponse(result);
-          })();
+            break;
+          }
+          case BackgroundActiontype.verify_proof: {
+            (async () => {
+              console.log('verify_proof', request.data);
+              // const result = await verify(request.data);
+              // sendResponse(result);
+            })();
 
-          return true;
-        }
-        case BackgroundActiontype.verify_prove_request: {
-          (async () => {
-            const result = await verify(request.data.proof);
+            return true;
+          }
+          case BackgroundActiontype.verify_prove_request: {
+            (async () => {
+              const proof: Proof = request.data.proof;
+              const result: { sent: string; recv: string } =
+                await verifyProof(proof);
 
-            if (result) {
               chrome.runtime.sendMessage<any, string>({
                 type: BackgroundActiontype.finish_prove_request,
                 data: {
@@ -159,17 +122,176 @@ const Offscreen = () => {
                   },
                 },
               });
-            }
-          })();
-          break;
+            })();
+            break;
+          }
+          default:
+            break;
         }
-        default:
-          break;
-      }
-    });
+      });
+    })();
   }, []);
 
   return <div className="App" />;
 };
 
 export default Offscreen;
+
+function subtractRanges(
+  ranges: { start: number; end: number },
+  negatives: { start: number; end: number }[],
+): { start: number; end: number }[] {
+  const returnVal: { start: number; end: number }[] = [ranges];
+
+  negatives
+    .sort((a, b) => (a.start < b.start ? -1 : 1))
+    .forEach(({ start, end }) => {
+      const last = returnVal.pop()!;
+
+      if (start < last.start || end > last.end) {
+        console.error('invalid ranges');
+        return;
+      }
+
+      if (start === last.start && end === last.end) {
+        return;
+      }
+
+      if (start === last.start && end < last.end) {
+        returnVal.push({ start: end, end: last.end });
+        return;
+      }
+
+      if (start > last.start && end < last.end) {
+        returnVal.push({ start: last.start, end: start });
+        returnVal.push({ start: end, end: last.end });
+        return;
+      }
+
+      if (start > last.start && end === last.end) {
+        returnVal.push({ start: last.start, end: start });
+        return;
+      }
+    });
+
+  return returnVal;
+}
+
+async function createProof(options: {
+  url: string;
+  notaryUrl: string;
+  websocketProxyUrl: string;
+  method?: string;
+  headers: {
+    [name: string]: string;
+  };
+  body?: any;
+  maxSentData?: number;
+  maxRecvData?: number;
+  id: string;
+  secretHeaders: string[];
+  secretResps: string[];
+}): Promise<ProofV1> {
+  const {
+    url,
+    method,
+    headers,
+    body,
+    maxSentData,
+    maxRecvData,
+    notaryUrl,
+    websocketProxyUrl,
+    id,
+    secretHeaders,
+    secretResps,
+  } = options;
+
+  const hostname = urlify(url)?.hostname || '';
+  const notary = NotaryServer.from(notaryUrl);
+  const prover: _Prover = await new Prover({
+    id,
+    serverDns: hostname,
+    maxSentData,
+    maxRecvData,
+  });
+
+  await prover.setup(await notary.sessionUrl(maxSentData, maxRecvData));
+
+  await prover.sendRequest(websocketProxyUrl + `?token=${hostname}`, {
+    url,
+    method,
+    headers,
+    body,
+  });
+
+  const transcript = await prover.transcript();
+
+  const commit = {
+    sent: subtractRanges(
+      transcript.ranges.sent.all,
+      secretHeaders
+        .map((secret: string) => {
+          const index = transcript.sent.indexOf(secret);
+          return index > -1
+            ? {
+                start: index,
+                end: index + secret.length,
+              }
+            : null;
+        })
+        .filter((data: any) => !!data) as { start: number; end: number }[],
+    ),
+    recv: subtractRanges(
+      transcript.ranges.recv.all,
+      secretResps
+        .map((secret: string) => {
+          const index = transcript.recv.indexOf(secret);
+          return index > -1
+            ? {
+                start: index,
+                end: index + secret.length,
+              }
+            : null;
+        })
+        .filter((data: any) => !!data) as { start: number; end: number }[],
+    ),
+  };
+
+  const session: _NotarizedSession = await new NotarizedSession(
+    await prover.notarize(commit),
+  );
+
+  const proofHex = await session.proof(commit);
+  const proof: ProofV1 = {
+    version: '1.0',
+    meta: {
+      notaryUrl,
+      websocketProxyUrl,
+    },
+    data: proofHex,
+  };
+  return proof;
+}
+
+async function verifyProof(
+  proof: Proof,
+): Promise<{ sent: string; recv: string }> {
+  let result: { sent: string; recv: string };
+
+  switch (proof.version) {
+    case undefined: {
+      result = await verify(proof);
+      break;
+    }
+    case '1.0': {
+      const tlsProof: _TlsProof = await new TlsProof(proof.data);
+      result = await tlsProof.verify({
+        typ: 'P256',
+        key: await NotaryServer.from(proof.meta.notaryUrl).publicKey(),
+      });
+      break;
+    }
+  }
+
+  return result;
+}
