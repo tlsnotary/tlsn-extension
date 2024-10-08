c4edbfd90d0a2c826a53a8f38e94afba89da9f2f
tsukino <0xtsukino@gmail.com>
2024-10-08 23:37:31 +0800

    feat: add options menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,92 @@
+import React, {
+  MouseEventHandler,
+  ReactElement,
+  useCallback,
+  useState,
+} from 'react';
+import Icon from '../Icon';
+import browser from 'webextension-polyfill';
+import classNames from 'classnames';
+import { useNavigate } from 'react-router';
+
+export function MenuIcon(): ReactElement {
+  const [opened, setOpen] = useState(false);
+
+  const toggleMenu = useCallback(() => {
+    setOpen(!opened);
+  }, [opened]);
+
+  return (
+    <div className="relative">
+      {opened && (
+        <>
+          <div
+            className="fixed top-0 left-0 w-screen h-screen z-10"
+            onClick={toggleMenu}
+          />
+          <Menu opened={opened} setOpen={setOpen} />
+        </>
+      )}
+      <Icon
+        fa="fa-solid fa-bars"
+        className="text-slate-500 hover:text-slate-700 active:text-slate-900 cursor-pointer z-20"
+        onClick={toggleMenu}
+      />
+    </div>
+  );
+}
+
+export default function Menu(props: {
+  opened: boolean;
+  setOpen: (opened: boolean) => void;
+}): ReactElement {
+  const navigate = useNavigate();
+  const openExtensionInPage = () => {
+    props.setOpen(false);
+    browser.tabs.create({
+      url: `chrome-extension://${chrome.runtime.id}/popup.html`,
+    });
+  };
+
+  return (
+    <div className="absolute top-[100%] right-0 rounded-md z-20">
+      <div className="flex flex-col bg-slate-200 shadow rounded-md py-1">
+        <MenuRow fa="fa-solid fa-toolbox" label="Plugins" />
+        <MenuRow
+          className="lg:hidden"
+          fa="fa-solid fa-up-right-and-down-left-from-center"
+          label="Expand"
+          onClick={openExtensionInPage}
+        />
+        <MenuRow
+          fa="fa-solid fa-gear"
+          label="Options"
+          onClick={() => {
+            props.setOpen(false);
+            navigate('/options');
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function MenuRow(props: {
+  fa: string;
+  label: string;
+  onClick?: MouseEventHandler;
+  className?: string;
+}): ReactElement {
+  return (
+    <div
+      className={classNames(
+        'flex flex-row items-center py-2 px-4 gap-2 hover:bg-slate-300 cursor-pointer text-slate-800 hover:text-slate-900',
+        props.className,
+      )}
+      onClick={props.onClick}
+    >
+      <Icon size={0.875} fa={props.fa} />
+      <div className="font-semibold">{props.label}</div>
+    </div>
+  );
+}
