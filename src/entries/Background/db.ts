bae2991d9cef1a9677f1a3cc6bdf23cd1d569e2e
tsukino <0xtsukino@gmail.com>
2025-01-15 22:40:41 +0800

    fix: make backward compatible with hostname pattern

--- src/entries/Background/db.ts
+++ src/entries/Background/db.ts
@@ -1,7 +1,8 @@
 import { Level } from 'level';
 import type { RequestHistory } from './rpc';
-import { PluginConfig, PluginMetadata, sha256 } from '../../utils/misc';
+import { PluginConfig, PluginMetadata, sha256, urlify } from '../../utils/misc';
 import mutex from './mutex';
+import { minimatch } from 'minimatch';
 const charwise = require('charwise');
 
 export const db = new Level('./ext-db', {
@@ -336,7 +337,19 @@ export async function getCookies(link: string, name: string) {
 
 export async function getCookiesByHost(link: string) {
   const ret: { [key: string]: string } = {};
-  for await (const [key, value] of cookiesDb.sublevel(link).iterator()) {
+  const links: { [k: string]: boolean } = {};
+  const url = urlify(link);
+
+  for await (const sublevel of cookiesDb.keys({ keyEncoding: 'utf8' })) {
+    const l = sublevel.split('!')[1];
+    links[l] = true;
+  }
+
+  const cookieLink = url
+    ? Object.keys(links).filter((l) => minimatch(l, link))[0]
+    : Object.keys(links).filter((l) => urlify(l)?.host === link)[0];
+
+  for await (const [key, value] of cookiesDb.sublevel(cookieLink).iterator()) {
     ret[key] = value;
   }
   return ret;
@@ -384,7 +397,19 @@ export async function getHeaders(host: string, name: string) {
 }
 export async function getHeadersByHost(link: string) {
   const ret: { [key: string]: string } = {};
-  for await (const [key, value] of headersDb.sublevel(link).iterator()) {
+  const url = urlify(link);
+
+  const links: { [k: string]: boolean } = {};
+  for await (const sublevel of headersDb.keys({ keyEncoding: 'utf8' })) {
+    const l = sublevel.split('!')[1];
+    links[l] = true;
+  }
+
+  const headerLink = url
+    ? Object.keys(links).filter((l) => minimatch(l, link))[0]
+    : Object.keys(links).filter((l) => urlify(l)?.host === link)[0];
+
+  for await (const [key, value] of headersDb.sublevel(headerLink).iterator()) {
     ret[key] = value;
   }
   return ret;
