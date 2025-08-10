1856062bc4f52f8af4170de4e433c4487011ab80
baptistapedro <gitinhabaptista@outlook.com>
2025-08-10 02:40:18 +0200

    Update index.ts

--- src/entries/Background/index.ts
+++ src/entries/Background/index.ts
@@ -37,40 +37,6 @@ import { installPlugin } from './plugins/utils';
     removeRequestLogsByTabId(tabId);
   });
 
-  const { defaultPluginsInstalled } = await getAppState();
-
-  switch (defaultPluginsInstalled) {
-    case false: {
-      try {
-        const twitterProfileUrl = browser.runtime.getURL(
-          'twitter_profile.wasm',
-        );
-        const discordDmUrl = browser.runtime.getURL('discord_dm.wasm');
-        await installPlugin(twitterProfileUrl);
-        await installPlugin(discordDmUrl);
-      } finally {
-        await setDefaultPluginsInstalled('0.1.0.703');
-      }
-      break;
-    }
-    case true: {
-      try {
-        await removePlugin(
-          '6931d2ad63340d3a1fb1a5c1e3f4454c5a518164d6de5ad272e744832355ee02',
-        );
-        const twitterProfileUrl = browser.runtime.getURL(
-          'twitter_profile.wasm',
-        );
-        await installPlugin(twitterProfileUrl);
-      } finally {
-        await setDefaultPluginsInstalled('0.1.0.703');
-      }
-      break;
-    }
-    case '0.1.0.703':
-      break;
-  }
-
   const { initRPC } = await import('./rpc');
   await createOffscreenDocument();
   initRPC();
