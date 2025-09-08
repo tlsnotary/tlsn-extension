4a3dc95d60676ce1f2e865b770cb3627596dca88
Kayanski <kowalski.kowalskin@gmail.com>
2025-09-08 10:26:14 +0200

    Another format removal

--- src/entries/Background/db.ts
+++ src/entries/Background/db.ts
@@ -357,7 +357,7 @@ export async function getPlugins(): Promise<
         ...config,
         hash,
         metadata: metadata
-         ? {
+          ? {
               ...metadata,
               hash,
             }
