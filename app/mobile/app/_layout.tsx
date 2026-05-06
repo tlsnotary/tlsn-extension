6eb49d6b90a634fcb795f66be215949f6d3719ea
tsukino <0xtsukino@gmail.com>
2026-05-06 21:53:19 +0800

    fix(mobile): polyfill crypto.getRandomValues for Hermes via react-native-get-random-values

--- app/mobile/app/_layout.tsx
+++ app/mobile/app/_layout.tsx
@@ -1,3 +1,4 @@
+import 'react-native-get-random-values';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
